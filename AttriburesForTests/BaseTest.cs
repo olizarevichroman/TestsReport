ef6135b114e37bad2de8c906a3fab4ab7f78dba4
olizarevichroman <olizarevich97@mail.ru>
2018-06-05 01:13:08 +0300

    refactored

--- AttriburesForTests/BaseTest.cs
+++ AttriburesForTests/BaseTest.cs
@@ -9,22 +9,12 @@ namespace AttriburesForTests
     [TestFixture]
     public class BaseTest
     {
-    private ReportHelper<TestReportModel> reportHelper = new ReportHelper<TestReportModel>();
-
-    [OneTimeTearDown]
-    protected void OneTimeTearDown()
-    {
-      //var testMethod = type.GetMethods(BindingFlags.Instance | BindingFlags.DeclaredOnly 
-      //  | BindingFlags.Public);
-
-      //reportHelper.Report(testMethod);
-      reportHelper.Report();
-    }
+    
 
     [TearDown]
     protected void TearDown()
     {
-      reportHelper.Add(TestContext.CurrentContext);
+      ReportHelper.Report(TestContext.CurrentContext);
     }
 
     }
--- AttriburesForTests/ReportHelper.cs
+++ AttriburesForTests/ReportHelper.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Reflection;
 using NUnit.Framework;
 using System.Linq;
+using System.Text;
 
 namespace AttriburesForTests
 {
@@ -12,63 +13,75 @@ namespace AttriburesForTests
   /// Provides an ability to write data from model to excel file with .xlsx extension
   /// </summary>
   /// <typeparam name="T">Type of model</typeparam>
-    public class ReportHelper<T> : IDisposable where T: new()
+    public static class ReportHelper
     {
-    private static bool isFirst = true;
-    //private static bool isSaved = false;
-    private static FileInfo fileInfo = new FileInfo($@"{Environment.CurrentDirectory}\..\..\..\report.xlsx");
-    private static ExcelPackage excelPackage;
-    private static ExcelWorksheet worksheet;
-    private List<T> listOfModels = new List<T>();
+    private static object locker = new object();
+    
+    private static string path = $@"{Environment.CurrentDirectory}\..\..\..\..\Report.html";
+
+    private static string table = "<table border=\"1\" style=\"border-collapse:collapse\">{0}</table>";
+
+    private static string row = "<tr>{0}</tr>";
+
+    private static string headerRow = "<tr style=\"font-weight:bold\">{0}</tr>";
+
+    private static string item = "<td>{0}</td>";
+
+    /// <summary>
+    /// Data inside the table
+    /// </summary>
+    private static StringBuilder allData;
+
+    private static PropertyInfo[] properties;
+
+    public static void Report(TestContext context)
+    {
+      WriteData(GetFormat(table, GetView(context)));
+    }
+
+    private static string GetView(TestContext context)
+    {
+      StringBuilder builder = new StringBuilder();
+      foreach(var property in properties)
+      {
+        builder.Append(GetFormat(item, context.Test.Properties[property.Name].FirstOrDefault()));
+      }
+      return allData.Append(GetFormat(row, builder.ToString())).ToString();
+    }
+
+    private static string GetFormat(string format, object value) => string.Format(format, value);
+
+    private static void WriteData(string data)
+    {
+      lock (locker)
+      {
+        using (StreamWriter writer = new StreamWriter(path))
+        {
+          writer.Write(data);
+        }
+      }
+    }
 
     static ReportHelper()
     {
-      excelPackage = new ExcelPackage(fileInfo);
-      worksheet = excelPackage.Workbook.Worksheets.Add("Report");
+      if (File.Exists(path))
+      {
+        File.Delete(path);
+      }
+      File.Create(path).Dispose();
+      properties = typeof(TestReportModel).GetProperties();
+      WriteHeaders();
     }
 
-    public void Dispose()
+    private static void WriteHeaders()
     {
-      excelPackage.Dispose();
-    }
-
-    public void Add(TestContext context)
-    {
-      listOfModels.Add(ModelConstructor<T>.GetModel(context));
-    }
-
-    public void Report()
-    {
-        worksheet.Cells.LoadFromCollection(listOfModels, isFirst);
-        if (isFirst) isFirst = false;
-      var address = worksheet.Cells.Address;
-        excelPackage.Save();
-      worksheet.Cells.Address = address;
-      //List<TestReportModel> model = new List<TestReportModel>();
-      //foreach (var context in testContexts)
-      //{
-      //  var newModel = new TestReportModel();
-      //  var type = Type.GetType(context.Test.ClassName);
-      //  string methodName = context.Test.MethodName;
-
-      //  var methodInfo = type.GetMethod(methodName);
-      //  var attributes =  methodInfo.GetCustomAttributes() ;
-
-      //}
-      //List<TestReportModel> list = new List<TestReportModel>();
-      //foreach ( var method in members)
-      //{
-      //  var attributes = method.CustomAttributes;
-      //  model = new TestReportModel
-      //  {
-      //    Fixture = attributes.ToArray()[1].ConstructorArguments[0].Value.ToString(),
-      //    TestId = attributes.ToArray()[2].ConstructorArguments[0].Value.ToString(),
-      //    UserStory = attributes.ToArray()[3].ConstructorArguments[0].Value.ToString()
-      //  };
-      //  list.Add(model);
-      //}
-      //worksheet.Cells.LoadFromCollection(list,true);
-      //excelPackage.SaveAs(new FileInfo(fileName: path));
+      StringBuilder builder = new StringBuilder();
+      foreach (var property in properties)
+      {
+        builder.Append(GetFormat(item, property.Name));
+      }
+      allData = new StringBuilder(GetFormat(headerRow, builder.ToString()));
+      WriteData(GetFormat(table, allData));
     }
 
     }
